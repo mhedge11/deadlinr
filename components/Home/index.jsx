49cb335dca7a32c9fb5f619d10618a3d49f7c513
thatprakhar <pnahar@purdue.edu>
2022-02-05 16:18:12 -0500

    added Home and Navigator

---
+++ components/Home/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (<View>
+            <Text>
+                Home
+            </Text>
+        </View>)
+    }
+}
---
+++ components/Navigator/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Navigator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (<View>
+            <Text>
+                Navigator
+            </Text>
+        </View>)
+    }
+}
