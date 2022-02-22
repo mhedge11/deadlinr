7fbaa2ccc61a64e029a967f409072e45418c6091
mhedge11 <mkhedge22@gmail.com>
2022-02-22 16:54:49 -0500

    addedJoinCalendarOnCalendarView;SeartchLightTheme

--- components/Calendar/CalendarView.jsx
+++ components/Calendar/CalendarView.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, Text, Switch, TouchableOpacity } from 'react-native';
-import { Icon } from 'react-native-elements'; 
+import React from "react";
+import { View, Text, Switch, TouchableOpacity, Button } from "react-native";
+import { Icon } from "react-native-elements";
+import JoinCalendar from "./JoinCalendar";
 
 /*
 
@@ -23,119 +24,117 @@ import { Icon } from 'react-native-elements';
 */
 
 export default class CalendarView extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      privateCalendar: this.props.route.params.isPrivate,
+      isMember: this.props.route.params.members.includes(this.props.user.uid),
+    };
+  }
 
-        this.state = {
-            privateCalendar: this.props.route.params.isPrivate,
-            isMember: this.props.route.params.members.includes(this.props.user.uid)
-        }
+  alterMemberStatus = () => {
+    if (this.state.isMember === true) {
+      // add API call to remove user from this calendar
+
+      this.setState({
+        isMember: false,
+      });
+    } else {
+      // add API call to add user to this calendar
+
+      this.setState({
+        isMember: true,
+      });
     }
+  };
 
-    alterMemberStatus = () => {
-        if (this.state.isMember === true) {
-            // add API call to remove user from this calendar
+  render() {
+    const { route, user, navigation } = this.props;
+    const { title, isPrivate, createrUID } = route.params;
 
-            this.setState({
-                isMember: false
-            })
-        } else {
-            // add API call to add user to this calendar
-
-            this.setState({
-                isMember: true
-            })
-        }
-    }
-
-
-    render() {
-        const { route, user, navigation } = this.props;
-        const { title, isPrivate, createrUID } = route.params;
-
-        
-
-        return (
-            <View
-                style={{
-                    padding: '10%',
-                    paddingTop: '20%',
-                }}
+    return (
+      <View
+        style={{
+          padding: "10%",
+          paddingTop: "20%",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            paddingTop: "5%",
+            justifyContent: "flex-start",
+            width: "100%",
+            overflow: "visible",
+          }}
+        >
+          <TouchableOpacity
+            style={{ justifyContent: "center" }}
+            onPress={() => navigation.goBack()}
+          >
+            <Icon name="chevron-left" type="font-awesome" color="black" />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: "30rem",
+              fontWeight: "600",
+              marginLeft: "10%",
+            }}
+          >
+            {title}
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              this.alterMemberStatus();
+            }}
+          >
+            <Text
+              style={{
+                color: this.state.isMember ? "red" : "green",
+                fontSize: "20rem",
+                fontWeight: "300",
+                marginLeft: "20%",
+                marginTop: "4%",
+              }}
             >
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        paddingTop: '5%',
-                        justifyContent: "flex-start",
-                        width: '100%',
-                        overflow: 'visible'
-                    }}
-                >
-                    <TouchableOpacity style={{ justifyContent: 'center' }} onPress={() => navigation.goBack()}>
-                        <Icon name='chevron-left' type='font-awesome' color='black'/>
-                    </TouchableOpacity>
-                    <Text
-                        style={{
-                            fontSize: '30rem',
-                            fontWeight: '600',
-                            marginLeft: '10%'
-                        }}
-                    >
-                        {title}
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => {
-                            this.alterMemberStatus();
-                        }}
-                    >
-                        <Text
-                            style={{
-                                color: this.state.isMember ? 'red' : 'green',
-                                fontSize: '20rem',
-                                fontWeight: '300',
-                                marginLeft: '20%',
-                                marginTop: '4%'
-                            }}
-                        >
-                            {
-                                this.state.isMember ? 'Leave Calendar' : 'Join Calendar'
-                            }
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-                
-                <View
-                    style={{
-                        // flexDirection: 'row',
-                        marginTop: '10%'
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontSize: '20rem'
-                        }}
-                    >
-                        Private
-                    </Text>
-                    <Switch
-                        disabled={createrUID !== user.uid}
-                        style={{
-                            marginTop: '3%'
-                        }}
-                        value={this.state.privateCalendar}
-                        onValueChange={() => {
-                            this.setState({
-                                privateCalendar: !this.state.privateCalendar
-                            })
-                            // add API call to change setting
-                        }}
-                        trackColor={{ false: "white", true: "#2776f5" }}
-                    />
-                </View>
+              {this.state.isMember ? "Leave Calendar" : "Join Calendar"}
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-            </View>
-        )
-    }
-
+        <View
+          style={{
+            // flexDirection: 'row',
+            marginTop: "10%",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: "20rem",
+            }}
+          >
+            Private
+          </Text>
+          <Switch
+            disabled={createrUID !== user.uid}
+            style={{
+              marginTop: "3%",
+            }}
+            value={this.state.privateCalendar}
+            onValueChange={() => {
+              this.setState({
+                privateCalendar: !this.state.privateCalendar,
+              });
+              // add API call to change setting
+            }}
+            trackColor={{ false: "white", true: "#2776f5" }}
+          />
+        </View>
+        <View>
+          <JoinCalendar />
+        </View>
+      </View>
+    );
+  }
 }
---
+++ components/Calendar/JoinCalendar.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import { SearchBar } from "react-native-elements";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  Platform,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  Alert,
+  Pressable,
+} from "react-native";
+import { Icon } from "react-native-elements";
+
+const JoinCalendar = (props) => {
+  const data = [
+    { id: "1", title: "Dr J" },
+    { id: "2", title: "MJ" },
+    { id: "3", title: "Chuck" },
+    { id: "4", title: "Shaq" },
+    { id: "5", title: "Kobe" },
+    { id: "6", title: "Reggie" },
+    { id: "7", title: "Lebron" },
+    { id: "8", title: "Steph" },
+    { id: "9", title: "KD" },
+  ];
+
+  const [search, setSearch] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("modal is closed");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.bar}>
+            <SearchBar
+              placeholder="Search here..."
+              onChangeText={updateSearch}
+              value={search}
+              lightTheme
+              round
+            />
+          </View>
+          <View>
+            <View style={[styles.modalView]}>
+              <View style={{ height: "80%", marginBottom: 30 }}>
+                <View style={[styles.textStyle, { alignItems: "center" }]}>
+                  <Text style={{ fontSize: 24, fontWeight: "bold", margin: 5 }}>
+                    Add People to Calendar
+                  </Text>
+                </View>
+                <FlatList
+                  data={data}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({ item }) => (
+                    <TouchableOpacity>
+                      <View style={[styles.textInput, { borderRadius: 20 }]}>
+                        <Text style={{ fontSize: 18 }}>{item.title}</Text>
+                      </View>
+                    </TouchableOpacity>
+                  )}
+                />
+              </View>
+              <Pressable
+                onPress={() => setModalVisible(!modalVisible)}
+                style={[styles.button, styles.modalClose]}
+              >
+                <Text style={styles.textStyle}>CLICK TO INVITE</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
+      <View
+        style={{
+          marginTop: 10,
+          padding: 20,
+          alignItems: "center",
+          //   backgroundColor: "grey",
+          width: "100%",
+          height: 100,
+        }}
+      >
+        <Pressable
+          onPress={() => setModalVisible(true)}
+          style={[
+            styles.button,
+            styles.modalOpen,
+            { backgroundColor: "black" },
+          ]}
+        >
+          <Icon name="paper-plane" type="font-awesome" color="white" />
+          <Text style={[{ color: "white" }]}>Invite</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    padding: 5,
+  },
+  textInput: {
+    marginTop: 10,
+    padding: 20,
+    alignItems: "center",
+    backgroundColor: "grey",
+    width: "100%",
+  },
+  bar: {
+    marginTop: 10,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "whitesmoke",
+    borderRadius: 20,
+    padding: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  modalOpen: {
+    backgroundColor: "black",
+  },
+  modalClose: {
+    backgroundColor: "seagreen",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 22,
+  },
+});
+
+export default JoinCalendar;
