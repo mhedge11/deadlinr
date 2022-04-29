e8fd85fe4f6ee56001c6703654b3ae5ce70581f5
thatprakhar <pnahar@purdue.edu>
2022-04-29 11:41:52 -0400

    notifications

--- Deadlinr.js
+++ Deadlinr.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
@@ -25,13 +25,82 @@ import CreateThread from './components/Threads/CreateThread';
 import CreateReplyToThread from './components/Threads/CreateReplyToThread';
 import CreateReplyToReply from './components/Threads/CreateReplyToReply';
 import { connect } from 'react-redux';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 const Stack = createNativeStackNavigator();
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+});
+
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
 class Deadlinr extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            expoPushToken: '',
+            notification: false
+        };
+        this.notificationListener = createRef();
+        this.responseListener = createRef();
+    }
+
+    componentDidMount() {
+        if (this.props.user == null || this.props.user == undefined) return;
+        registerForPushNotificationsAsync().then(token => this.setState({
+            expoPushToken: token
+        }));
+
+        // This listener is fired whenever a notification is received while the app is foregrounded
+        this.notificationListener.current = Notifications.addNotificationReceivedListener(notification => this.setState({
+            notification
+        }));
+    
+        // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+        this.responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+          console.log(response);
+        });
+
+        console.log(this.state.expoPushToken);
+    }
+
+    componentWillUnmount() { 
+        Notifications.removeNotificationSubscription(this.notificationListener.current);
+        Notifications.removeNotificationSubscription(this.responseListener.current);
     }
 
     render() {
